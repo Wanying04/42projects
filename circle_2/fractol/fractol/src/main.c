a332ae31e19e9f7b4f769982858c0765a9932f6e
wtang <wtang@student.42malaga.com>
2025-08-13 22:09:58 +0200

    20250813

--- circle_2/fractol/fractol/src/main.c
+++ circle_2/fractol/fractol/src/main.c
@@ -6,15 +6,40 @@
 /*   By: wtang <wtang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 23:20:57 by wtang             #+#    #+#             */
-/*   Updated: 2025/08/12 23:40:22 by wtang            ###   ########.fr       */
+/*   Updated: 2025/08/13 22:04:27 by wtang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include
+#include "../mlx_linux/mlx.h"
 
-int	main(int ac, char **av)
+typedef struct	s_data
 {
-	if (ac != 2)
-		show_help();
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	*img;
 	
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
