59f0daf5041177b8099e57676abd9b80232dcab2
wtang <wtang@student.42malaga.com>
2025-09-12 20:17:55 +0200

    20250912

--- circle_2/minitalk/minitalk/client.c
+++ circle_2/minitalk/minitalk/client.c
@@ -3,10 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wtang <wtang@student.42malaga.com>         +#+  +:+       +#+        */
+/*   By: wtang <wtang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 18:48:03 by wtang             #+#    #+#             */
-/*   Updated: 2025/07/27 18:48:04 by wtang            ###   ########.fr       */
+/*   Updated: 2025/09/12 20:16:33 by wtang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+volatile sig_atomic_t		g_ack_received = 0;
+
+void	ack_handler(int sig)
+{
+	if (sig == SIGUSR1)
+		g_ack_received = 1;
+	else if (sig == SIGUSR2)
+	{
+		printf("Message delivered successfully!\n");
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void	send_char(pid_t server_pid, char c)
+{
+	int	bit;
+	
+	bit = 7;
+	while (bit >= 0)
+	{
+		g_ack_received = 0;
+		
+		if ((c >> bit) & 1)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		usleep(500);
+		if (!g_ack_received)
+		{
+			printf("Error: No acknowledgement from server\n");
+			exit(EXIT_FAILURE);
+		}
+		bit--;
+	}
+}
+
+void	send_message(pid_t server_pid, const char *message)
+{
+	int	i;
+
+	struct sigaction	sa;
+	sa.sa_handler = ack_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (message[i])
+	{
+		send_char(server_pid, message[i]);
+		i++;
+	}
+	send_char(server_pid, '\0');
+	printf("Waiting for final acknowledgement...\n");
+	sleep(3);
+	printf("Error: Final acknowledgement timeout\n");
+	exit(EXIT_FAILURE);
+}
+
+int	main(int argc, char **argv)
+{
+	__pid_t	server_pid;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <server_pid> <message>\n", argv[0]);
+		printf("Example: %s 1234 \"Hello World\"\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	server_pid = atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		printf("Error: Invalid server PID\n");
+		return (EXIT_FAILURE);
+	}
+	printf("Client PID: %d\n", getpid());
+	printf("Sending to server PID: %d\n", server_pid);
+	printf("Message: %s\n", argv[2]);
+	send_message(server_pid, argv[2]);
+	return (EXIT_SUCCESS);
+}
--- circle_2/minitalk/minitalk/server.c
+++ circle_2/minitalk/minitalk/server.c
@@ -3,10 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wtang <wtang@student.42malaga.com>         +#+  +:+       +#+        */
+/*   By: wtang <wtang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 18:48:18 by wtang             #+#    #+#             */
-/*   Updated: 2025/07/27 18:48:19 by wtang            ###   ########.fr       */
+/*   Updated: 2025/09/12 19:55:30 by wtang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+static void	handle_signal(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_count = 0;
+	static char	current_char = 0;
+	static pid_t	client_pid = 0;
+	(void)context;
+
+	if (client_pid == 0)
+		client_pid = info->si_pid;
+	if (sig == SIGUSR1)
+		current_char = (current_char << 1) | 1;
+	else if (sig == SIGUSR2)
+		current_char = (current_char << 1) | 0;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		if (current_char == '\0')
+		{
+			write(1, "\n", 1);
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+		}
+		else
+		{
+			write(1, &current_char, 1);
+			kill(client_pid, SIGUSR1);
+		}
+		bit_count = 0;
+		current_char = 0;
+	}
+	else
+		kill(client_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	printf("Server PID: %d\n", getpid());
+	printf("Waiting for messages...\n");
+	sa.sa_sigaction = handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+		pause();
+	return (0);
+}
