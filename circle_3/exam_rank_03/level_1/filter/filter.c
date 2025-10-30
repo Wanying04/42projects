b8e023a44adce43b9239c5b5af43aeffb7f9df73
wanying04 <wanyingt@hotmail.com>
2025-10-30 17:46:09 +0100

    20251030

--- circle_3/exam_rank_03/level_1/filter/filter.c
+++ circle_3/exam_rank_03/level_1/filter/filter.c
@@ -1,46 +1,18 @@
-#define _GNU_SOURCE //memmem()
-#include <fcntl.h>  //read()
-#include <errno.h>  //perror()
-
-#define INITIAL_BUFFER_SIZE 4096
-#define BUFFER_GROWTH_FACTOR 2
+#include <stdio.h>
+#include <string.h>
 
 int	main(int ac, char **av)
 {
+	size_t	len;
+
 	if (ac != 2)
 		return (1);
 	if (av[1][0] == '\0')
 		return (1);
-
-	char	*pattern = av[1];
-	size_t	pattern_len = strlen(pattern);
-
-	char	*buffer = NULL;
-	size_t	buffer_size = 0;
-	size_t	data_len = 0;
-	
-	ssize_t	bytes_read;
-	int	ret_code = 0;
-	
-	buffer_size = INITIAL_BUFFER_SIZE;
-	buffer = malloc(buffer_size);
-	if (buffer = NULL)
+	len = strlen(av[1]);
+	malloc(sizeof(len));
+	while (av[1][i])
 	{
-		fprintf(stderr, "Error: ");
-		perror("malloc");
-		return (1);
-	}
-	
-	while (1)
-	{
-		if (data_len + pattern_len >= buffer_size)
-		{
-			size_t new_size = buffer_size * BUFFER_GROWTH_FACTOR;
-			char *new_buffer = realloc(buffer, new_size);
-			if (new_buffer = NULL)
-			{
-				
-			}
-		}
+
 	}
 }
