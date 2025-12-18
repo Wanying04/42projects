52391ffbb20d1812c5e66c167c502fc76a7cc3f5
wtang <wtang@student.42malaga.com>
2025-12-18 13:51:13 +0100

    20251218

--- circle_3/exam_rank_03/level_1/filter/filter.c
+++ circle_3/exam_rank_03/level_1/filter/filter.c
@@ -1,37 +1,35 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 int	main(int ac, char **av)
 {
-	int i = 0;
-	int leido = 1;
-	int len;
-	int j;
-	char buffer[99999];
+	char	buffer[100000];
+	int		i = 0;
+	int		leido = 1;
+	int		j;
+	int		len = 0;
 
-	if (ac != 2)
+	if (ac != 2)  // Parameter Check
 		return (1);
 	len = strlen(av[1]);
-	while (leido > 0)
+	while (leido > 0)  // read char by char into buffer
 		leido = read(0, &buffer[i++], 1);
-	buffer[--i] = 0;
+	buffer[--i] = 0;  // \0
 	i = 0;
-	while(buffer[i])
+	while (buffer[i])
 	{
-		j = 0;
+		j = 0;  // compare each char
 		while (buffer[i + j] && av[1][j] && buffer[i + j] == av[1][j])
 			j++;
-		if (j == len)
-		{
+		if (j == len)  // if they are the same
+		{	
 			while (j--)
-			{
+			{	
 				write(1, "*", 1);
 				i++;
 			}
 		}
-		else
+		else  // if they are different
 			write(1, &buffer[i++], 1);
 	}
 	return (0);
--- circle_3/exam_rank_03/level_2/rip/rip.c
+++ circle_3/exam_rank_03/level_2/rip/rip.c
@@ -1,81 +1,54 @@
 #include <stdio.h>
 
+// min number of () that need to be removed
 int	invalid(char *s)
 {
 	int i = 0;
-	int open = 0;
-	int close = 0;
+	int open = 0;  // unmatched left (
+	int close = 0;  // unmatched right )
 
 	while (s[i])
 	{
-		if (s[i] == '(')
+		if (s[i] == '(')  // find left (
 			open++;
-		else if (s[i] == ')')
+		else if (s[i] == ')')  // find right )
 		{
-			if (open > 0)
+			if (open > 0)  // match ( and )
 				open--;
 			else
-				close++;
+				close++;  // find unmatched right )
 		}
 		i++;
 	}
-	return (open + close);
+	return (open + close);  // all () that need to be removed
 }
 
-void result(char *s, int remove, int delete, int pos)
+void	result(char *s, int remove, int delete, int pos)
 {
+	//If enough () have been removed and the current str is balanced
 	if (remove == delete && !invalid(s))
 	{
 		puts(s);
 		return ;
 	}
-	while (s[pos])
+	while (s[pos])  // remove () from position pos
 	{
 		if (s[pos] == '(' || s[pos] == ')')
 		{
-			char c = s[pos];
-			s[pos] = ' ';
-			result(s, remove, delete + 1, pos + 1);
-			s[pos] = c;
+			char c = s[pos];  // save current char
+			s[pos] = ' ';  // replace with space
+			result(s, remove, delete + 1, pos + 1);  // recursive attempt
+			s[pos] = c;  // restore original char
 		}
 		pos++;
 	}
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac != 2)
 		return (1);
-	int remove = invalid(av[1]);
-	result(av[1], remove, 0, 0);
+	int remove = invalid(av[1]);  // min number of () that need to be removed
+	result(av[1], remove, 0, 0);  // recursive search
 	return (0);
 }
-
-/*Assignment name  : rip
-Expected files   : *.c *.h
-Allowed functions: puts, write
---------------------------------------------------------------------------------
-
-Escribe un programa que tome como argumento una cadena que contenga solo paréntesis.
-Si los paréntesis no están equilibrados (por ejemplo, «()»),
-tu programa deberá eliminar el número mínimo de paréntesis para que la expresión quede equilibrada.
-Por eliminar nos referimos a sustituirlos por espacios.
-A continuación, imprimirás todas las soluciones (puede haber más de una).
-
-El orden de las soluciones no es importante.
-
-Por ejemplo, esto debería funcionar:
-$> ./rip '(()' | cat -e
- ()$
-( )$
-$> ./rip '((()()())())' | cat -e
-((()()())())$
-$> ./rip '()())()'| cat -e
-()() ()$
-()( )()$
-( ())()$
-$> ./rip '(()(()(' | cat -e
-(()  ) $
-( )( ) $
-( ) () $
- ()( ) $*/
